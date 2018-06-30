04f0e864b039e477eb2d285b48d2e246bd7779c2
Alex Bruce <brucealmightyalex@yahoo.com>
2018-06-30 17:59:28 -0600

    Add most testing functionality

--- test/Log-test.js
+++ test/Log-test.js
@@ -8,7 +8,11 @@ describe('Log', function () {
     log = new Log(625, 250, 80, 30, '#d2691e', -1, 1);
   });
 
-  it('should start back at the right side when it reaches the left side', function() {
-    assert.equal(log.checkLogBounds(), true);
+  it('should start back at the right side when it reaches the left side', function () {
+    assert.equal(log.x, 625);
+    log.checkLogBounds();
+    assert.equal(log.x, 625);
+    log.x = -81;
+    log.checkLogBounds(log.x, 600);
   });
 });
--- test/Vehicle-test.js
+++ test/Vehicle-test.js
@@ -1,7 +1,36 @@
 const { assert } = require('chai');
 const Vehicle = require('../lib/Vehicle.js');
+const Frog = require('../lib/Frog.js');
 
 
 describe('Vehicle', function() {
-  const vehicle = new Vehicle()
+  var vehicle;
+  var frog;
+
+  beforeEach('initialize vehicle', function () {
+    frog = new Frog(280, 560, 40, 40, 'rgb(0, 200, 0)');
+    vehicle = new Vehicle(100, 321, 80, 38, '#999', 1, 1);
+  });
+
+  it('should be able to collide with the frog', function () {
+    frog.x = 100;
+    frog.y = 321;
+    const isColliding = vehicle.isCollidingWith(frog);
+
+    assert.isTrue(isColliding);
+  });
+
+  it('should not collide with the frog when they don\'t overlap', function () {
+    const isColliding = vehicle.isCollidingWith(frog);
+
+    assert.isFalse(isColliding);
+  });
+
+  it('should start back at the left side when it reaches the right side', function () {
+    assert.equal(vehicle.x, 100);
+    vehicle.checkVehicleBounds();
+    assert.equal(vehicle.x, 100);
+    vehicle.x = 601;
+    vehicle.checkVehicleBounds(vehicle.x, 520);
+  });
 })
