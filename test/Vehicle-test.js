0b424a22b253211916d122edebff3a7b52e6c334
Alex Bruce <brucealmightyalex@yahoo.com>
2018-07-03 15:02:04 -0600

    Add tests to Frog, Log, and Vehicle to testobject properties

--- test/Vehicle-test.js
+++ test/Vehicle-test.js
@@ -12,6 +12,19 @@ describe('Vehicle', function() {
     vehicle = new Vehicle(null, 100, 321, 80, 38, 1, 1);
   });
 
+  it('should have properties', function () {
+    const vehicle = new Vehicle(null, 50, 50, 10, 10, 1);
+    const expectedObj = {
+      image: null,
+      x: 50,
+      y: 50,
+      height: 10,
+      width: 10,
+      dx: 1,
+      dxv: 0
+    }
+  });
+
   it('should be able to collide with the frog', function () {
     frog.x = 100;
     frog.y = 321;
